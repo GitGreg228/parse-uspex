0833eed30ed91c22f8a21ab9b1b0e01200d6d448
GitGreg228 <shutov.gm@phystech.edu>
2022-05-11 17:43:34 +0300

    now ZPE can create all relaxation inputs

---
+++ inputs.py
@@ -0,0 +1,89 @@
+import os
+import re
+
+
+def get_system(number):
+    system_1 = {
+        "partition": "lenovo",
+        "modules": "module load vasp/6.1.1",
+        "N": 1,
+        "n": 8,
+        "mpirun": "$(which mpirun)",
+        "npar": 4,
+    }
+
+    system_2 = {
+        "partition": "cpu",
+        "modules": "module load ",
+        "N": 1,
+        "n": 8,
+        "mpirun": "$(which mpirun)",
+        "npar": 4,
+    }
+
+    if number == 1:
+        return system_1
+    if number == 2:
+        return system_2
+
+
+def compose_potcar(structure, specific_path, dir_name):
+    formula = structure.formula
+    split = re.split(r'(\d+)', formula)
+    potcar_paths = list()
+    for specie in split:
+        specie = specie.replace(' ', '')
+        if specie.isalpha():
+            potcar = os.path.join(specific_path, f'POTCAR_{specie}')
+            assert os.path.isfile(potcar)
+            potcar_paths.append(potcar)
+    with open(os.path.join(dir_name, 'POTCAR'), 'w') as outfile:
+        for potcar in potcar_paths:
+            with open(potcar, 'r') as infile:
+                for line in infile:
+                    outfile.write(line)
+
+
+def get_slurm_script(properties, system, path):
+    script = f"""#!/bin/sh
+#SBATCH -o out -e err
+#SBATCH -p {system['partition']}
+#SBATCH -J r{properties['formula']}
+#SBATCH -N {system['N']}
+#SBATCH -n {system['n']}
+
+{system['modules']}
+
+{system['mpirun']} $(which vasp_std) > log
+
+""".format()
+
+    with open(os.path.join(path, 'script.sh'), 'w') as f:
+        f.write(script)
+
+
+def write_incar(option, properites, pressure, path):
+    relaxation_incar = f"""SYSTEM = EA{properites['id']} {properites['stability']} {properites['formula']} {properites['space_group']}
+PREC = Normal
+ENCUT = 600
+EDIFF = 1e-8
+IBRION = 2
+ISIF = 3
+NSW = 666
+ISMEAR = 1 ; SIGMA = 0.2
+POTIM = 0.250
+ISTART = 0
+LCHARG = FALSE
+LWAVE = FALSE
+KSPACING = 0.314
+PSTRESS = {pressure}
+NPAR = 4
+""".format()
+
+    if option == 'relaxation':
+        incar = relaxation_incar
+    elif option == 'phonons:':
+        pass
+
+    with open(os.path.join(path, 'INCAR'), 'w') as f:
+        f.write(incar)
--- zpe.py
+++ zpe.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from utils import listdirs, analyze_symmetry, split_poscars, parse_ech
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.core.structure import IStructure
+from inputs import *
 
 
 def main():
@@ -15,7 +16,10 @@ def main():
     parser.add_argument('--tol_min', type=float, default=0.01, help='Minimum tolerance')
     parser.add_argument('--tol_step', type=float, default=0.01, help='Tolerance step')
     parser.add_argument('--tol_max', type=float, default=0.5, help='Maximum tolerance')
+    parser.add_argument('--system', type=int, default=1, help='Slurm config in inputs.py')
+    parser.add_argument('--press', type=int, default=2000, help='pressure in kBar for subsequent VASP calculations')
     args = parser.parse_args()
+    system = get_system(args.system)
 
     full_dirs = list()
     for dir in listdirs(args.path):
@@ -25,31 +29,51 @@ def main():
     print(f'Will work in {output}')
     for dir in full_dirs:
         print(f'Working in {dir} ...')
+        specific_path = os.path.join(dir, '../Specific/')
         poscars = split_poscars(dir)
         zpe_path = os.path.join(dir, 'ZPE')
         if not os.path.isdir(zpe_path):
             os.mkdir(zpe_path)
         structures = parse_ech(dir, args.ths, poscars, args.tol_min, args.tol_step, args.tol_max, dump_dir=zpe_path)
         for comp in structures.keys():
-            comp_path = os.path.join(zpe_path, comp)
-            if not os.path.isdir(comp_path):
-                os.mkdir(comp_path)
+            # comp_path = os.path.join(zpe_path, comp)
+            # if not os.path.isdir(comp_path):
+            #     os.mkdir(comp_path)
             for cat in structures[comp].keys():
                 if cat and comp:
                     print(f'Working with the {comp} {cat} structures')
                     for structure in structures[comp][cat]:
                         symmetry = structure['symmetry']
-                        space_group = symmetry[list(symmetry)[-1]][0]
+                        space_group = int(symmetry[list(symmetry)[-1]][0])
                         formula = structure['formula']
                         if cat == 'stable':
-                            name = f'{cat}_{formula}_{space_group}'
+                            stability = cat
                         else:
-                            name = f'{structure["fitness"]}_{formula}_{space_group}'
-                        dir_name = os.path.join(comp_path, name)
+                            stability = structure["fitness"]
+                        properties = {
+                            'formula': formula,
+                            'space_group': space_group,
+                            'id': structure["id"],
+                            'stability': stability
+                                      }
+
+                        name = f'EA{structure["id"]}_{stability}_{formula}_{space_group}'
+                        dir_name = os.path.join(zpe_path, name)
                         if not os.path.isdir(dir_name):
                             os.mkdir(dir_name)
                         tmp_structure = IStructure.from_dict(structure['structure'])
-                        analyze_symmetry(tmp_structure, args.tol_min, args.tol_step, args.tol_max, save_dir=dir_name)
+                        analyzer = SpacegroupAnalyzer(tmp_structure, symprec=args.tol)
+                        primitive = analyzer.get_primitive_standard_structure()
+                        poscar = os.path.join(dir_name, 'POSCAR')
+                        primitive.to(filename=poscar, fmt='POSCAR')
+                        with open(poscar, 'r') as f:
+                            lines = f.readlines()
+                        with open(poscar, 'w') as f:
+                            lines[0] = f'EA{structure["id"]} {stability} {formula} {space_group}'
+                            f.writelines(lines)
+                        compose_potcar(tmp_structure, specific_path, dir_name)
+                        get_slurm_script(properties, system, dir_name)
+                        write_incar('relaxation', properties, args.press, dir_name)
 
 
 if __name__ == '__main__':
